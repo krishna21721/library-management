d7155bb23c07e54d841582bef0fa837523acb968
ronakAtArkay <ronak@arkayapps.com>
2024-02-29 10:59:58 +0000

    Add code in  Books PY

--- library_management/library_management/doctype/books/books.py
+++ library_management/library_management/doctype/books/books.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2024, Krishna and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Books(Document):
-	pass
+	def validate(self):
+		if not self.name1:
+			frappe.throw("Please add Book Name")
